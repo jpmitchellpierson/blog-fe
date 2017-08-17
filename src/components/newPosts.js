d1de993e0c41814ae4b9dacba0b37967a0110410
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-08-17 16:00:49 -0700

    redux forms

--- src/components/newPosts.js
+++ src/components/newPosts.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+// reduxForm is a function that helps connect this component
+// to communicate with the formReducer we added in reducer index
+import { Field, reduxForm } from 'redux-form';
 
 class NewPosts extends Component {
   render() {
     return (
-      <div>
-        New Posts
-      </div>
+      <form>
+        <Field 
+          name="title"
+          component={}
+        />
+      </form>
     );
   };
 };
 
-export default NewPosts;
+export default reduxForm({
+  form: 'NewPostsForm'
+})(NewPosts);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducerPosts';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  // all different forms in different components will assume
+  // the formReducer is being applied to the form piece of state
+  form: formReducer
 });
 
 export default rootReducer;
