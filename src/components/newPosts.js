e75206ed58ddd3da097a60554c4d53f0157e8ee9
jpmitchellpierson <jpmitchellpierson@gmail.com>
2017-08-17 15:12:37 -0700

    rendering posts

---
+++ src/components/newPosts.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewPosts extends Component {
+  render() {
+    return (
+      <div>
+        New Posts
+      </div>
+    );
+  };
+};
+
+export default NewPosts;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/postsIndex';
+import NewPosts from './components/newPosts';
 
 // wire up redux promise as middleware inside app
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
@@ -15,7 +16,11 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
+        <Switch>
+          {/* order paths from most specific to most general to only render one route at a time. Use Switch component to switch routes and only render first path that matches exactly  */}
+          <Route path="/posts/new" component={NewPosts} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
